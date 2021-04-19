894b3157979150009fcca9907095b0d9ebb3d9cc
Rafael De Luca <deluca1712@gmail.com>
2021-04-18 22:12:36 -0300

    Exercicio padaria

--- padaria-associacao/src/Doces.java
+++ padaria-associacao/src/Doces.java
@@ -1,4 +1,3 @@
-
 public class Doces extends Produto {
 
 	private String nome;
--- padaria-associacao/src/Produto.java
+++ padaria-associacao/src/Produto.java
@@ -1,41 +1,43 @@
 
-public abstract class Produto  {
-	
-	// um produto não pode ser instanciodo só com preço e quantidade, tem que ser instanciado com um atributo nome
-	// através das subClasses							
-	
+public abstract class Produto {
+
+	// um produto não pode ser instanciodo só com preço e quantidade, tem que ser
+	// instanciado com um atributo nome
+	// através das subClasses
+
 	private double preco;
-	private int quantidade;	
-	
-	public Produto () {
-		
-	}	
-	public Produto(double preco, int quantidade) {
-		this.preco=preco;
-		this.quantidade=quantidade;
-		
+	private int quantidade;
+
+	public Produto() {
+
 	}
+
+	public Produto(double preco, int quantidade) {
+		this.preco = preco;
+		this.quantidade = quantidade;
+
+	}
+
 	public double getPreco() {
 		return preco;
 	}
+
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
+
 	public int getQuantidade() {
 		return quantidade;
 	}
+
 	public void setQuantidade(int quantidade) {
 		this.quantidade = quantidade;
 	}
-	
+
 	public double totalPedido() {
-		double total=0;
+		double total = 0;
 		total += quantidade * preco;
 		return total;
 	}
-	
-		
-	
+
 }
-
-
